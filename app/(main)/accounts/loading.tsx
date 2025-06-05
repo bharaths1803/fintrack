28797d3ac35312c4ac58c8e317f4cbe11a5124f4
bharaths1803 <bharaths14051803@gmail.com>
2025-06-06 00:46:59 +0530

    Added loading.tsx to all pages

---
+++ app/(main)/accounts/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-white">
+      <div className="h-10 w-10 border-4 border-gray-200 border-t-primary-600 rounded-full animate-spin" />
+    </div>
+  );
+}
